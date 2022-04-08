3e33c100db6dd5ab810d8dd5e21bbd79376645d5
RKatana <roduor41@gmail.com>
2022-04-08 14:23:43 +0300

    done with work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    QuotesComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
---
+++ src/app/quotes/quotes.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
+@Component({
+  selector: 'app-quotes',
+  templateUrl: './quotes.component.html',
+  styleUrls: ['./quotes.component.css']
+})
+export class QuotesComponent implements OnInit {
+
+  public quotes : Quote[] =[
+    new Quote(1, 'First Quote', 1, 0),
+    new Quote(2, 'Second Quote', 3, 0),
+    new Quote(3, 'Third Quote', 0, 0)
+  ]
+
+  arr: number[] = this.quotes.map(quote=>quote.upvotes)
+  highest = Math.max(...this.arr)
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
